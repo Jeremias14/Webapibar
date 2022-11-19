b6514f6ac483f46fdf37965dbc3fcb541933d580
Jeremias14 <jererodrsac@gmail.com>
2022-11-19 15:17:28 -0300

    Actualizar Customer

--- Dtos/CustomerUpdateDto.cs
+++ Dtos/CustomerUpdateDto.cs
@@ -8,6 +8,7 @@ namespace WebApiBar.Dtos
     public class CustomerPutDto
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public string Address { get; set; }
         public string Cellphone { get; set; }
         
